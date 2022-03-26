f855a58b5b946cb57934df2eb86541b1130421c5
Juari5antos <developando.mange@gmail.com>
2022-03-26 09:34:49 -0300

    Second commit v.1

--- home/models.py
+++ home/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=30)
+    cpf = models.CharField(max_length=15)
+    status = models.CharField(
+        max_length=100,
+        choices=(
+            ('A', 'Ativo'),
+            ('I', 'Bloqueado'),
+            ('C', 'Cancelado'),
+        )
+    )
+
+    def --str - -(self):
+
+    return self.none
+
+class Curso(models.Model):
+    curso = models.CharField(max_length=30)
+    descricao = models.CharField(max_length=100)
+    status = models.CharField(
+        max_length=100,
+        choices=(
+            ('A', 'Ativo'),
+            ('I', 'Inativo'),
+        )
+    )
+
+    def --str - -(self):
+
+    return self.none
